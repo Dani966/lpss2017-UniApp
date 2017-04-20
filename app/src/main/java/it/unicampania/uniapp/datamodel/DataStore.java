acf53824ace85151a8c11e4dbd080c31ca45a13b
pcantiello <pasquale.cantiello@unicampania.it>
2017-04-20 15:09:10 +0200

    Aggiunto il data model con gli studenti

---
+++ app/src/main/java/it/unicampania/uniapp/datamodel/DataStore.java
@@ -0,0 +1,102 @@
+package it.unicampania.uniapp.datamodel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe che rappresenta il datamodel dell'applicazione.
+ * Si dovrà interfacciare con il database
+ * Prima versione: simulazione con dati locali
+ * Created by pasquale on 20/04/17.
+ */
+
+class DataStore {
+
+    // Lista locale degli studenti
+    // Todo: da eliminare
+    private ArrayList<Studente> studenti;
+
+    /**
+     * Costruttore
+     */
+    public DataStore() {
+        studenti = new ArrayList<>();
+    }
+
+    /**
+     * Aggiunge uno studente al database
+     * @param studente studente da aggiungere
+     */
+    public void aggiungiStudente(Studente studente) {
+        studenti.add(studente);
+    }
+
+    /**
+     * Aggiorna i dati dello studente utilizzando la matricola come riferimento
+     * @param studente dati da aggiornare
+     */
+    public void aggiornaStudente(Studente studente) {
+        int posizione = getStudenteIndex(studente.getMatricola());
+        if (posizione == -1)
+            aggiungiStudente(studente);
+        else
+            studenti.set(posizione, studente);
+    }
+
+    /**
+     * Elimina uno studente
+     * @param matricola matricola dello studente da eliminare
+     */
+    public void eliminaStudente(String matricola) {
+        int posizione = getStudenteIndex(matricola);
+        if (posizione != -1)
+            studenti.remove(posizione);
+    }
+
+    /**
+     * Legge lo studente individuato dalla matricola passata
+     * @param matricola matricola da cercare
+     * @return Studente letto, oppure null nel caso non venga trovato
+     */
+    public Studente leggiStudente(String matricola) {
+        int posizione = getStudenteIndex(matricola);
+        if (posizione == -1)
+            return null;
+        else
+            return studenti.get(posizione);
+    }
+
+    /**
+     * Ottiene l'elenco di tutti gli studenti
+     * Todo: Attenzione il metodo è potenzialmente pericoloso. Potrebbe restituire troppi dati!
+     * @return Lista di studenti
+     */
+    public List<Studente> elencoStudenti() {
+        return studenti;
+    }
+
+    /**
+     * Restituisce il numero di studenti presenti nel database
+     * @return numero di studenti
+     */
+    public int numeroStudenti() {
+        return studenti.size();
+    }
+
+    /**
+     * Restituisce l'indice di uno studente nell'array partendo dalla matricola
+     * @param matricola matricola da cercare
+     * @return indice dello studente. -1 se non trovato
+     */
+    private int getStudenteIndex(String matricola) {
+        boolean trovato = false;
+        int index = 0;
+        while (!trovato && index < studenti.size()) {
+            if (studenti.get(index).getMatricola().equals(matricola)) {
+                return index;
+            }
+            ++index;
+        }
+        return -1;
+    }
+}
---
+++ app/src/main/java/it/unicampania/uniapp/datamodel/Studente.java
@@ -0,0 +1,66 @@
+package it.unicampania.uniapp.datamodel;
+
+/**
+ * Generica classe
+ * Created by pasquale on 20/04/17.
+ */
+
+class Studente {
+
+    private String matricola;
+    private String cognome;
+    private String nome;
+    private int crediti;
+
+    /**
+     * Costruttore
+     */
+    public Studente() {
+    }
+
+    /**
+     * Costruttore
+     * @param matricola matricola dello studente
+     * @param cognome cognome
+     * @param nome nome di battesimo
+     * @param crediti maturati
+     */
+    public Studente(String matricola, String cognome, String nome, int crediti) {
+        this.matricola = matricola;
+        this.cognome = cognome;
+        this.nome = nome;
+        this.crediti = crediti;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCrediti() {
+        return crediti;
+    }
+
+    public void setCrediti(int crediti) {
+        this.crediti = crediti;
+    }
+}
