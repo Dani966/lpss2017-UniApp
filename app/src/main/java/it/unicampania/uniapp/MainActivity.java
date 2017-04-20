dd28180bc6575440921de04ef986ba9c1dd357b1
pcantiello <pasquale.cantiello@unicampania.it>
2017-04-20 15:23:12 +0200

    Creati i layout di lista e riga

--- app/src/main/java/it/unicampania/uniapp/MainActivity.java
+++ app/src/main/java/it/unicampania/uniapp/MainActivity.java
@@ -2,12 +2,19 @@ package it.unicampania.uniapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Widget
+    private ListView listaStudenti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaStudenti = (ListView)findViewById(R.id.listaStudenti);
+
     }
 }
---
+++ app/src/main/java/it/unicampania/uniapp/StudentiAdapter.java
@@ -0,0 +1,33 @@
+package it.unicampania.uniapp;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Adapter per la gestione degli studenti
+ * Created by pasquale on 20/04/17.
+ */
+
+class StudentiAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
