6c1021ac5dd5d0a52ce8f630f567f5c71b6b33ab
pcantiello <pasquale.cantiello@unicampania.it>
2017-05-13 11:03:54 +0200

    Abilitato Firebase

--- app/src/main/java/it/unicampania/uniapp/MainActivity.java
+++ app/src/main/java/it/unicampania/uniapp/MainActivity.java
@@ -2,11 +2,17 @@ package it.unicampania.uniapp;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import it.unicampania.uniapp.datamodel.DataStore;
 import it.unicampania.uniapp.datamodel.Studente;
 
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Costanti
     private final static String EXTRA_STUDENTE = "studente";
+    private final static String TAG = "UniApp";
 
     // Widget
     private ListView listaStudenti;
@@ -24,10 +31,28 @@ public class MainActivity extends AppCompatActivity {
     // Data Store
     private DataStore archivio = new DataStore();
 
+    // Autenticazione Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Autenteciazione Firebase
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "login effettuato: " + user.getUid());
+                } else {
+                    Log.d(TAG, "Logout effettuato");
+                }
+            }
+        };
 
         listaStudenti = (ListView)findViewById(R.id.listaStudenti);
         adapter = new StudentiAdapter(this);
@@ -42,5 +67,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
     }
 }
